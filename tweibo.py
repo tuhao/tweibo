1a554e36d2561e5f6d384da7c11a0b1d4e76ec67
Ikfly <astro.liuhang@gmail.com>
2014-01-14 00:09:16 +0800

    search

--- tweibo.py
+++ tweibo.py
@@ -1,11 +1,16 @@
 #coding=utf-8
 import ConfigParser
 import urllib
-import sys
 import urllib2
 import json
 import datetime
 import time
+from ttypes import *
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+import DataService
+import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
@@ -35,6 +40,7 @@ class Tweibo:
 			oauth_consumer_key=cfg('oauth_consumer_key'),
 			openid=cfg('openid'),
 			)
+		self.content = None
 	
 	def search(self,query):
 		self.params.update(query)
@@ -47,44 +53,74 @@ class Tweibo:
 		body = urllib.urlencode(self.params)
 		req = urllib2.Request( self.base_url + '?' + body, None, headers)
 		f=urllib2.urlopen(req)
-		self.generate(f.read())
-	
-	def generate(self,content):
-		ret = json.loads(content)
+		self.content = f.read()
+
+class ThriftClient:
+
+	def __init__(self):
+		self.host = cfg('thrift_host')
+		self.port = 9090
+		self.socket = TSocket.TSocket(self.host,self.port)
+		self.transport = TTransport.TFramedTransport(self.socket)
+		self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
+		self.client = DataService.Client(self.protocol)
+		self.msg = list()
+
+
+	def generate(self,str_json):
+		ret = json.loads(str_json)
 		data = ret.get('data',{})
 		if data is None:
-			print data
+			print ret
 		else:
 			self.hasnext = str(data.get('hasnext',None))
-			print 'hasnext:%s' % (self.hasnext)
 			items = data.get('info',[])
-			with open('food.txt','a') as f:
-				for item in items:
-					print item['text'].encode('utf-8')
-					f.write(item['text'].encode('utf-8') + '\n')
-					pics = item.get('pic',None)
-					if pics:
-						for image in pics['info']:
-							print ''.join(image['url']) + '/460.jpg'
-							f.write(''.join(image['url']) + '/460.jpg' + '\n')
+			for item in items:
+				pics = item.get('pic',None)
+				image_url = None
+				if pics is not None:
+					for image in pics['info']:
+						image_url = ''.join(image['url']) + '/460.jpg'
+						break
+					if image_url is not None:
+						content = str(item['text'] + image_url).encode('utf-8')
+						message = Message(title='tweibo',content=content)
+						self.msg.append(message)
 
-starttime = datetime.datetime.now() - datetime.timedelta(hours =1 )
+
+
+	def send(self):
+		if len(self.msg) > 0:
+			for message in self.msg:
+				print message.content
+				print '\n*****************************\n'
+			self.transport.open()
+			print self.client.pushMsg(self.msg)
+			self.transport.close()
+			self.msg = list()
+	
+
+starttime = datetime.datetime.now() - datetime.timedelta(minutes =5 )
 starttime = int(time.mktime(starttime.timetuple()))
 startpage = 1
 timestamp = int(time.time())
 weibo = Tweibo()
+thrift_client = ThriftClient()
 
 while True:
 	print startpage
 	query = dict(
 			keyword='美食',
 			page=startpage,
-			pagesize=30,
+			pagesize=2,
 			starttime=starttime,
 			endtime=timestamp,)
 	weibo.search(query)
+	thrift_client.generate(weibo.content)
+	thrift_client.send()
+
 	startpage = startpage + 1
 	if weibo.hasnext == '2' or weibo.hasnext == '0':
 		print 'weibo.hasnext %s ' % (weibo.hasnext)
 		break
-	time.sleep(3)
+	break
