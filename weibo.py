a7c4716ad0cee89f6712a8d39bd41191e1c8f93e
unknown <astro.liuhang@gmail.com>
2014-05-30 18:23:46 +0800

    weibo

--- weibo.py
+++ weibo.py
@@ -11,18 +11,17 @@ class Weibo:
 	def __init__(self):
 		self.key = 'q'
 		self.url = 'http://s.weibo.com/wb/'
+		self.params = dict()
+		self.content = None
 
 	def search(self,keyword):
-		key = 'q'
-		params = {}
-		params[key]= u'美食'.encode('utf-8')
-		query =  urllib.urlencode(params)
-		params[key] = query[len(key) + 1:].encode('utf-8')
-		query = urllib.urlencode(params)
-		url = 'http://s.weibo.com/wb/' + query[len(key) + 1:]
-		print url
+		self.params.update({self.key:keyword})
+		query = urllib.urlencode(self.params)
+		self.params[self.key] = query[len(self.key) + 1:].encode('utf-8')
+		query = urllib.urlencode(self.params)
+		self.url += query[len(self.key) + 1:]
+		print self.url
 		headers = {'Referer':'http://weibo.com',
 					'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-		req = urllib2.Request(url, None, headers)
-		content = urllib2.urlopen(req).read()
-		print content
+		req = urllib2.Request(self.url, None, headers)
+		self.content = urllib2.urlopen(req).read()
